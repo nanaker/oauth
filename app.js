ffd00f3b07f2bc02473db9964e37bf82f2a82ef9
nanaker <en_kerkar@esi.dz>
2018-03-04 22:18:25 +0100

    Update app.js

--- app.js
+++ app.js
@@ -37,6 +37,6 @@ app.get('/oauth/info', oauth2.info);// pour avoir les infos du token // autoriza
 
 
 // create server
-http.createServer(app).listen(8000, () => {
-  console.log(`Express HTTP server listening on port 8000`);
+http.createServer(app).listen(8080, () => {
+  console.log(`Express HTTP server listening on port 8080`);
 });
